d6865820c0b10e6f9407258274efccb7bc9932cd
silverdoggos <55032919+silverdoggos@users.noreply.github.com>
2019-12-17 17:29:04 +0300

    Initial Commit

---
+++ spotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spotter
+//
+//  Created by Артём Шишкин on 17.12.2019.
+//  Copyright © 2019 Артём Шишкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
